96a6127e49e2aedfef9ce722d6a2ddaf870dd5c9
oplol <hsini4ever@gmail.com>
2015-07-24 18:05:05 -0400

    starting leetcode

---
+++ 001.java
@@ -0,0 +1,27 @@
+// Given an array of integers, find two numbers such that they add up to a specific target number.
+
+// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+
+// You may assume that each input would have exactly one solution.
+
+// Input: numbers={2, 7, 11, 15}, target=9
+// Output: index1=1, index2=2
+
+// my solution:
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        outter:
+        for (int i = 0; i < nums.length; i++) {
+            int left = target - nums[i];
+            result[0] = i + 1;
+            for (int j = i + 1; j < nums.length; j++ ) {
+                if (left == nums[j]) {
+                    result[1] = j + 1;
+                    break outter;
+                }
+            }
+        }
+        return result;
+    }
+}
